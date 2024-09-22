7b0ecb3bdac7e077528098417ce1468cb7d127b6
tugrulsimsirli <simsirlitugrul@gmail.com>
2024-09-23 02:53:00 +0300

    init rabbitmq service

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/tugrulsimsirli/rabbitmq
+
+go 1.23
+
+require github.com/streadway/amqp v1.1.0
+
---
+++ rabbitmq_service.go
@@ -0,0 +1,86 @@
+package rabbitmq
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQService struct {
+	Connection *amqp.Connection
+	Channel    *amqp.Channel
+	Queue      amqp.Queue
+}
+
+// NewRabbitMQService creates a new instance of RabbitMQService
+func NewRabbitMQService(rabbitmqURL, queueName string) (*RabbitMQService, error) {
+	conn, err := amqp.Dial(rabbitmqURL)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName, // queue name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RabbitMQService{
+		Connection: conn,
+		Channel:    ch,
+		Queue:      q,
+	}, nil
+}
+
+// Publish sends a message to the RabbitMQ queue
+func (r *RabbitMQService) Publish(message string) error {
+	err := r.Channel.Publish(
+		"",           // exchange
+		r.Queue.Name, // routing key (queue name)
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	if err != nil {
+		return err
+	}
+
+	log.Printf(" [x] Sent %s", message)
+	return nil
+}
+
+// Consume listens for messages from the RabbitMQ queue
+func (r *RabbitMQService) Consume() (<-chan amqp.Delivery, error) {
+	msgs, err := r.Channel.Consume(
+		r.Queue.Name, // queue
+		"",           // consumer
+		true,         // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
+	)
+	if err != nil {
+		return nil, err
+	}
+	return msgs, nil
+}
+
+// Close closes the RabbitMQ connection and channel
+func (r *RabbitMQService) Close() {
+	r.Channel.Close()
+	r.Connection.Close()
+}
